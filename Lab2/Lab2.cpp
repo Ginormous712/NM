25c7875cbe522f221275d8824e4d54f5bcffe658
Ginormous712 <romavitaliy470@gmail.com>
2023-11-10 13:47:09 +0200

    Finished lab2

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -13,13 +13,20 @@ int main()
         {0.0, 5.0, 2.0, 3.0},
         {0.0, 1.0, 2.0, 7.0}
     };
-
+    ///*
     vector<vector<double>> data2 = {
         {1.0, 2.0, 0.0},
         {2.0, 2.0, 4.0},
         {0.0, 4.0, 3.0}
     };
-
+    //*/
+    /*
+    vector<vector<double>> data2 = {
+        {1.0, 1.0, 0.0},
+        {1.0, 3.0, 2.0},
+        {0.0, 1.0, 2.0}
+    };
+    */
     vector<vector<double>> data3 = {
         {4.0, 0.0, 1.0, 1.0},
         {0.0, 3.0, 0.0, 1.0},
@@ -29,10 +36,17 @@ int main()
 
     vector<double> b1 = { 14,31,33,45 };
     vector<double> b2 = { 11,34,31 };
+    //vector<double> b2 = { 1,1,1 };
     vector<double> b3 = { 11,10,7,23 };
+
+    int maxIterations = 10;
+    double epsilon = 0.001;
+
+    //cin >> epsilon >> maxIterations;
 
     try 
     {
+        
         cout << SPACE;
         cout << "Gaussian method" << endl;
         SquareMatrix a(data1);
@@ -57,16 +71,29 @@ int main()
         cout << "Tridiagonal matrix algorithm" << endl;
         TridiagonalMatrix b(data2);
         cout << SPACE;
+        cout << "Matrix B:\n" << b;
+        cout << SPACE;
         cout << "Solving linear system using Tridiagonal matrix algorithm:\n";
         vector<double> result2 = b.tridiagonalSolve(b2);
         b.solveAndPrint(b2, result2);
-
-
         
+        cout << SPACE;
+        cout << "Gauss-Seidel method" << endl;
+        SquareMatrix c(data3);
+        cout << SPACE;
+
+        cout << "Matrix C:\n" << c;
+        cout << SPACE;
+
+        cout << "Solving linear system using Gauss-Seidel method:\n";
+        vector<double> result3 = c.gaussSeidel(b3, maxIterations, epsilon);  
+        //cout << c.isSymmetric();
+        //cout << c.determinant();
     }
     catch (const invalid_argument& e) 
     {
         cerr << "Error: " << e.what() << endl;
     }
+   
 
 }
--- Lab2/Matrix.h
+++ Lab2/Matrix.h
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-//TODO 2 and 3 task
+
 class Matrix 
 {
 protected:
@@ -189,6 +190,11 @@ public:
         return column;
     }
 
+    vector<vector<double>> getData() const
+    {
+        return data;
+    }
+
     void gaussianSimpleElimination()
     {
         for (int i = 0; i < rows; i++)
@@ -241,6 +247,11 @@ public:
                     maxVal = abs(data[k][i]);
                     maxRow = k;
                 }
+            }
+
+            if (maxVal == 0.0)
+            {
+                throw std::runtime_error("Matrix has no unique solution");
             }
 
             if (maxRow != i)
@@ -314,6 +325,23 @@ public:
         cout << endl;
     }
 
+    Matrix transpose() const 
+    {
+        int rows = data.size();
+        int cols = data[0].size();
+
+        Matrix result(vector<vector<double>>(cols, vector<double>(rows, 0.0)));
+
+        for (int i = 0; i < rows; ++i) 
+        {
+            for (int j = 0; j < cols; ++j) 
+            {
+                result.data[j][i] = data[i][j];
+            }
+        }
+
+        return result;
+    }
 };
 
 
@@ -347,6 +375,14 @@ public:
         return det;
     }
     
+    bool isSingular()
+    {
+        double d = determinant();
+        double epsilon = 1e-10;
+
+        return abs(d) < epsilon;
+    }
+
     vector<double> solveLinearSystemGauss(vector<double> b)
     {
         if (rows != b.size())
@@ -396,8 +432,9 @@ public:
         Matrix augmetedMatrix(augmented);
         augmetedMatrix.gaussianElimination();
 
-        SquareMatrix inverseMatrix(n);
 
+        SquareMatrix inverseMatrix(n);
+        
         for (int i = 0; i < n; i++) 
         {
             for (int j = 0; j < n; j++) 
@@ -408,7 +445,116 @@ public:
 
         return inverseMatrix;
     }
+
+    static double calculateEuclideanNorm(const vector<double>& vector) 
+    {
+        double norm = 0.0;
+        for (double component : vector) {
+            norm += component * component;
+        }
+        return sqrt(norm);
+    }
+
+    static double calculateAbsoluteError(const vector<double>& x1, const vector<double>& x2)
+    {
+        int size = x1.size();
+        double error = 0.0;
+
+        for (int i = 0; i < size; i++) 
+        {
+            error = max(error, abs(x1[i] - x2[i]));
+        }
+
+        return error;
+    }
+
+    bool isSymmetric() const
+    {
+        Matrix transposed = transpose();
+        return data == transposed.getData();
+    }
+
+    bool isConvergent() const
+    {
+        int size = data.size();
+        for (int i = 0; i < size; i++)
+        {
+            double diagonalElement = abs(data[i][i]);
+            double sum = 0.0;
+            for (int j = 0; j < size; ++j)
+            {
+                if (j != i)
+                {
+                    sum += abs(data[i][j]);
+                }
+            }
+            if (diagonalElement <= sum)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    vector<double> gaussSeidel(const vector<double>& b, int maxIterations, double epsilon) const 
+    {
+        int n = rows;
+        vector<double> x(n, 0.0);
+
+        cout << "Vector b:\n";
+        printVector(b, false);
+
+        double e = epsilon;
+        int pow = 0;
+        while (e != 1)
+        {
+            pow++;
+            e *= 10;
+        }
+
+        cout << setprecision(pow) << fixed;
+        double error1 = 0;
+        for (int iter = 0; iter < maxIterations; iter++) 
+        {
+            vector<double> x_old = x;
+
+            for (int i = 0; i < n; i++) 
+            {
+                double sigma = 0.0;
+
+                for (int j = 0; j < n; j++) 
+                {
+                    if (j != i) 
+                    {
+                        sigma += data[i][j] * x[j];
+                    }
+                }
+
+                x[i] = (b[i] - sigma) / data[i][i];
+            }
+            cout << iter + 1 << "| ";
+            printVector(x, true);
+            double error2 = calculateEuclideanNorm(x);
+            cout << "Error: " << error2 - error1 << endl;
+            //double error = calculateAbsoluteError(x, x_old);
+            if (abs(error2 - error1) < epsilon) 
+            {
+                cout << "*****************     STOP     ******************\n";
+                cout << "Result in " << iter + 1 << " iterations.\n";
+                cout.unsetf(ios::fixed);
+                return x;
+            }
+            error1 = error2;
+        }
+
+        cout << "We reached the maximum number of iterations.\n";
+        cout.unsetf(ios::fixed);
+        return x;
+    }
 };
+
 
 class TridiagonalMatrix : public SquareMatrix
 {
@@ -421,8 +567,56 @@ public:
         }
     }
     
+    static bool checkStability(vector<vector<double>> data) 
+    {
+        vector<double> a, b, c;
+        a.reserve(data[0].size());
+        b.reserve(data[0].size());
+        c.reserve(data[0].size());
+
+        for (int i = 0; i < data[0].size(); i++)
+        {
+            if (i > 0)
+            {
+                a.push_back(data[i][i - 1]);
+            }
+            else
+            {
+                a.push_back(0.0);
+            }
+
+            b.push_back(data[i][i]);
+
+            if (i < data[0].size() - 1)
+            {
+                c.push_back(data[i][i + 1]);
+            }
+            else
+            {
+                c.push_back(0.0);
+            }
+        }
+
+        bool flag = false;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (abs(b[i]) < abs(a[i]) + abs(c[i]))
+            {
+                return false;
+            }
+            if (abs(b[i]) > abs(a[i]) + abs(c[i]))
+            {
+                flag = true;
+            }
+
+        }
+        return flag;
+    }
+    /*
     vector<double> tridiagonalSolve(const vector<double>& d) const 
     {
+        
+
         if (rows != d.size()) {
             throw invalid_argument("The dimension of the right-hand side vector does not correspond to the dimension of the matrix");
         }
@@ -454,7 +648,10 @@ public:
                 c.push_back(0.0);
             }
         }
-
+        if (!checkStability(a, b, c))
+        {
+            throw std::runtime_error("The tridiagonal matrix is not diagonally dominant, and the stability condition is not satisfied.");
+        }
         vector<double> alpha(rows);
         vector<double> beta(rows);
         vector<double> x(rows);
@@ -477,8 +674,47 @@ public:
 
         return x;
     }
+    //*/
+    
+    vector<double> tridiagonalSolve(const vector<double>& d) const 
+    {
+        if (rows != d.size() || rows != data[0].size() || rows != data.size()) 
+        { 
+            throw invalid_argument("Matrix and vector dimensions do not match");
+        }
+        /*
+        if (!checkStability(getData()))
+        {
+            throw std::runtime_error("The tridiagonal matrix is not diagonally dominant, and the stability condition is not satisfied.");
+        }
+        */
+        vector<double> alpha(rows);
+        vector<double> beta(rows);
+        vector<double> z(rows);
 
+        alpha[0] = -data[0][1] / data[0][0];
+        beta[0] = d[0] / data[0][0];
 
+        for (int i = 1; i < rows - 1; i++) 
+        {
+            z[i] = -data[i][i] - alpha[i - 1] * data[i][i - 1];
+            alpha[i] = data[i][i + 1] / z[i];
+            beta[i] = (-d[i] + data[i - 1][i] * beta[i - 1]) / z[i];
+            
+            
+        }
+        z[rows - 1] = -data[rows - 1][rows - 1] - alpha[rows - 2] * data[rows - 1][rows - 2];
+
+        vector<double> x(rows);
+        x[rows - 1] = (-d[rows-1] + data[rows - 1][rows - 2]*beta[rows-2])/z[rows - 1];
+
+        for (int i = rows - 2; i >= 0; i--) 
+        {
+            x[i] = alpha[i] * x[i + 1] + beta[i];
+        }
+
+        return x;
+    }
 
 };
 
